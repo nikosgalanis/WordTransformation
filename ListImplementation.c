c10e7f308d998adc73dcd11b4ae06549643591f8
Nikos Galanis <ngalanis99@gmail.com>
2019-03-18 08:22:44 +0200

    Add files via upload

--- ListImplementation.c
+++ ListImplementation.c
@@ -3,26 +3,6 @@
 #include <stdlib.h>
 #include "ListInterface.h"
 
-void InsertNewLastNode(char *w, NodeType **L) {
-      NodeType *N, *P;
-      int w_length = strlen(w);
-      N = (NodeType *)malloc(sizeof(NodeType));
-      N->word = malloc((w_length+1)*sizeof(char));
-      strcpy(N->word, w);
-      N->Link = NULL;
-
-      if (*L == NULL) {
-         *L = N;
-      } else {
-         P = *L;
-         while (P->Link != NULL) P = P->Link;
-         P->Link = N;
-      }
-
-}
-
-
-
 void PrintList(NodeType *L) {
       NodeType *N;
 
@@ -45,31 +25,11 @@ char* DeleteFirstNode(NodeType** L) {
       first = malloc((l+1)*sizeof(char));
       strcpy(first,(*L)->word);
       (*L) = (*L)->Link;
-      // free(temp);
+      free((temp)->word);
+      free(temp->Link);
     }
     return first;
 }
-
-void DeleteLastNode(NodeType **L) {
-      NodeType *PreviousNode, *CurrentNode;
-
-      if (*L != NULL) {
-         if ((*L)->Link == NULL) {
-            free(*L);
-            *L = NULL;
-         } else {
-            PreviousNode = *L;
-            CurrentNode = (*L)->Link;
-            while (CurrentNode->Link != NULL){
-               PreviousNode = CurrentNode;
-               CurrentNode = CurrentNode->Link;
-            }
-            PreviousNode->Link = NULL;
-            free(CurrentNode);
-         }
-      }
-}
-
 
 void InsertFirst(char* w,NodeType **L) {
       NodeType *N;
@@ -83,24 +43,10 @@ void InsertFirst(char* w,NodeType **L) {
 
 int Length(NodeType* L){
   int count = 0;
-  NodePointer temp = L;
+  NodeType* temp = L;
   while(temp != NULL){
     count++;
     temp = temp->Link;
   }
   return count;
-}
-
-NodeType *ListSearch(char *w, NodeType *L) {
-      NodeType *N;
-
-      N = L;
-      while (N != NULL) {
-        if (strcmp(N->word,w) == 0) {
-           return N;
-        } else {
-           N = N->Link;
-        }
-      }
-      return N;
 }
--- ListInterface.h
+++ ListInterface.h
@@ -4,8 +4,5 @@
 /* function prototypes */
 int Length(NodeType*);
 void InsertFirst(char*, NodeType **);
-void InsertLast(char *, NodeType **);
-void DeleteLastNode(NodeType **);
 char* DeleteFirstNode(NodeType** L);
-NodeType *ListSearch(char *, NodeType *);
 void PrintList(NodeType *);
