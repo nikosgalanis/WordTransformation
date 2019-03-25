cd97e2308bb7487d2642ab3c7b15e770d251cca1
Nikos Galanis <ngalanis99@gmail.com>
2019-03-25 13:09:22 +0200

    Delete functions.h

--- functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "PQInterface.h"
-typedef char* word;
-
-int cmpfunc(const void *, const void *);
-word change(word, int, char);
-NodePointer Similar(word, word*, int,word,int*);
-void Convert(word, word, word*, int);
