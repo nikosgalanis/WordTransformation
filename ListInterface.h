ab3911c598eaaa5fd2d02f4a81591f7383ca37cf
Nikos Galanis <ngalanis99@gmail.com>
2019-03-25 13:07:35 +0200

    Delete ListInterface.h

--- ListInterface.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ListTypes.h"
-
-/* function prototypes */
-int Length(NodeType*);
-void InsertFirst(char*, NodeType **);
-char* DeleteFirstNode(NodeType** L);
-void PrintList(NodeType *);
